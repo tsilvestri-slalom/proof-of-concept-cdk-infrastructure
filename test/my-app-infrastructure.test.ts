18f134d1bf4cb21efe036e4974681147ae20ab54
Tony Silvestri <tonys@slalom.com>
2025-09-06 07:58:12 -0400

    Initial commit

---
+++ test/my-app-infrastructure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MyAppInfrastructure from '../lib/my-app-infrastructure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/my-app-infrastructure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MyAppInfrastructure.MyAppInfrastructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
